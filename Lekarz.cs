cbf178997538e15c704460f4b6a73e2c41a77d7e
Kacper Majcherski <kacpermajcherski@gmail.com>
2026-01-23 00:01:22 +0100

    22.01.2026-23.01.2026

--- Lekarz.cs
+++ Lekarz.cs
@@ -8,13 +8,26 @@ namespace Szpital
 {
     internal class Lekarz : Pracownik
     {
-        public string Specjalizacja { get; set; }
+        public enum SpecjalizacjaRodzaj
+        {
+            Kardiolog,
+            Urolog,
+            Neurolog,
+            Laryngolog
+        }
+
+        public SpecjalizacjaRodzaj Specjalizacja { get; set; }
+
         public string NumerPWZ { get; set; }
 
-        public Lekarz(string imie, string nazwisko, string nazwaUzytkownika, string haslo, string pesel, string specjalizacja, string numerPWZ) : base(imie, nazwisko, nazwaUzytkownika, haslo, pesel)
+        public Lekarz(string imie, string nazwisko, string nazwaUzytkownika, string haslo, string pesel, SpecjalizacjaRodzaj specjalizacja, string numerPWZ) : base(imie, nazwisko, nazwaUzytkownika, haslo, pesel)
         {
             Specjalizacja = specjalizacja;
             NumerPWZ = numerPWZ;
-        }
+            
+
+    }
+        public List<Dyzur> Dyżury { get; set; } = new List<Dyzur>();
+
     }
 }
--- Pracownik.cs
+++ Pracownik.cs
@@ -6,13 +6,14 @@ using System.Threading.Tasks;
 
 namespace Szpital
 {
-    internal class Pracownik
+    public abstract class Pracownik
     {
         public string Imie { get; set; }
         public string Nazwisko { get; set; }
         public string Pesel { get; set; }
         public string NazwaUzytkownika { get; }
         public string Haslo { get; }
+        public abstract string Rola { get; }
 
         public Pracownik(string imie, string nazwisko, string nazwaUzytkownika, string haslo, string pesel)
         {
